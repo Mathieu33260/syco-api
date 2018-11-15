60884bf1fc959dd6b6871118671cb845841f42ad
Dumez Mathieu <mathieu.dumez@etu.u-bordeaux.fr>
2018-11-15 21:59:21 +0100

    first connect

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var request = require("request");
+var mysql = require('mysql');
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+
+    //todo set authorization headers (api key)
+
+    var con = mysql.createConnection({
+        host: "hebfree.org",
+        user: "siteppp",
+        password: "iutaudessus33",
+        database: "siteppp0"
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        sql =
+            "SELECT * " +
+            "FROM syco";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Result: " + result);
+        });
+    });
 });
 
 module.exports = router;
